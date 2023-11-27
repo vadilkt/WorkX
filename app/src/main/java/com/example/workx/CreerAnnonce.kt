151cb84b00535bcb2643921fd01ce56ab6225cc8
TIEUBEA <borismoussa5@gmail.com>
2023-11-27 03:38:05 +0100

    Annonce OK

---
+++ app/src/main/java/com/example/workx/CreerAnnonce.kt
@@ -0,0 +1,11 @@
+package com.example.workx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreerAnnonce : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creer_annonce)
+    }
+}
