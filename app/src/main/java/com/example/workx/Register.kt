7fe2bec76acf6c0e186cb0bf7776e74fb124bcbe
Vadil <65923431+vadilkt@users.noreply.github.com>
2023-11-29 19:57:26 +0100

    Auth Ok

--- app/src/main/java/com/example/workx/Register.kt
+++ app/src/main/java/com/example/workx/Register.kt
@@ -5,15 +5,15 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.Button
-import model.UserData
+import com.example.workx.model.UserData
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -42,14 +42,14 @@ class Register : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var firebaseDatabase: FirebaseDatabase
-    @RequiresApi(Build.VERSION_CODES.P)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         signUpProgress = findViewById(R.id.signUp_Progress)
         etLastName = findViewById(R.id.etLastName)
-        etFirstName= findViewById(R.id.etFirstName)
+        etFirstName = findViewById(R.id.etFirstName)
         etEmail = findViewById(R.id.etEmail)
         etPassword = findViewById(R.id.etPassword)
         etCPassword = findViewById(R.id.etConfirmPassword)
@@ -62,110 +62,153 @@ class Register : AppCompatActivity() {
         firebaseDatabase = FirebaseDatabase.getInstance()
         databaseRef = firebaseDatabase.getReference("UserData")
 
-        viewLogin.setOnClickListener{
+        viewLogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
         etButton.setOnClickListener {
-            if(!validateLastName() || !validateFirstName() || !validateEmail() || !validatePhone() || !validatePass()){
+            if (!validateLastName() || !validateFirstName() || !validateEmail() || !validatePhone() || !validatePass()) {
                 return@setOnClickListener
             }
 
-            if (etPassword.text.toString()== etCPassword.text.toString()){
+            if (etPassword.text.toString() == etCPassword.text.toString()) {
                 signUpProgress.visibility = View.VISIBLE
                 mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener{task: Task<AuthResult> ->
-                        try{
-                            if(task.isSuccessful){
-                                val data = UserData(firstname,lastname, email, phonenumber)
+                    .addOnCompleteListener { it ->
+                        try {
+                            if (it.isSuccessful) {
+                                Toast.makeText(
+                                    this,
+                                    "Enregistrement en cours...",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                 val data = UserData(
+                                     etFirstName.text.toString().trim(),
+                                     etLastName.text.toString().trim(),
+                                     etEmail.text.toString().trim(),
+                                     etPhoneNumber.text.toString().trim()
+                                 )
                                 FirebaseDatabase.getInstance().getReference("UserData")
                                     .child(FirebaseAuth.getInstance().currentUser?.uid!!)
                                     .setValue(data)
-                                    .addOnCompleteListener{it->
-                                         signUpProgress.visibility = View.GONE
-
-                                        if(it.isSuccessful){
-                                            Toast.makeText(this, "Enregistrement Réussi !", Toast.LENGTH_SHORT).show()
+                                    .addOnCompleteListener { it ->
+                                        signUpProgress.visibility = View.GONE
+                                        if (it.isSuccessful) {
+                                            Toast.makeText(
+                                                this,
+                                                "Enregistrement Réussi !",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
                                             val intent = Intent(this, Accueil::class.java)
                                             startActivity(intent)
                                             finish()
+                                        } else {
+                                            Toast.makeText(
+                                                this,
+                                                "Enregistrement échoué !",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
                                         }
                                     }
-                            } else{
+                                    .addOnFailureListener { e ->
+                                        //
+                                        Log.e(
+                                            "Firebase",
+                                            "Erreur lors de l'enregistrement dans la base de données",
+                                            e
+                                        )
+                                    }
+                            } else {
                                 signUpProgress.visibility = View.GONE
-                                Toast.makeText(this, "Vérifier votre email ou votre mot de passe !", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    this,
+                                    "Vérifier votre email ou votre mot de passe !",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
-                        }catch(e : Exception){
+                        } catch (e: Exception) {
                             e.printStackTrace()
                         }
                     }
-            } else{
-                Toast.makeText(this, "Les mots de passe ne correspondent pas !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Les mots de passe ne correspondent pas !", Toast.LENGTH_SHORT)
+                    .show()
 
             }
         }
     }
 
-    private fun validateFirstName(): Boolean{
+    private fun validateFirstName(): Boolean {
         firstname = etFirstName.text.toString().trim()
-        return if(TextUtils.isEmpty(firstname)){
+        return if (TextUtils.isEmpty(firstname)) {
             Toast.makeText(this, "Entrez votre Nom", Toast.LENGTH_SHORT).show()
             false
-        } else{
-            true
-        }
-    }
-    private fun validateLastName(): Boolean{
-        lastname = etLastName.text.toString().trim()
-        return if(TextUtils.isEmpty(lastname)){
-            Toast.makeText(this, "Entrez votre Prénom", Toast.LENGTH_SHORT).show()
-            false
-        } else{
-            true
-        }
-    }
-    private fun validateEmail(): Boolean{
-        email = etEmail.text.toString().trim()
-        return if(TextUtils.isEmpty(email)){
-            Toast.makeText(this, "Entrez votre adresse mail !", Toast.LENGTH_SHORT).show()
-            false
-        } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            Toast.makeText(this, "Veuillez entrer une adresse mail valide !", Toast.LENGTH_SHORT).show()
-            false
-        } else{
-            true
-        }
-    }
-    private fun validatePhone(): Boolean{
-        phonenumber = etPhoneNumber.text.toString().trim()
-        return if(TextUtils.isEmpty(phonenumber)){
-            Toast.makeText(this, "Entrez votre Nom", Toast.LENGTH_SHORT).show()
-            false
-        } else if(!Patterns.PHONE.matcher(phonenumber).matches()){
-            Toast.makeText(this, "Veuillez entrer un numéro de téléphone valide !", Toast.LENGTH_SHORT).show()
-            false
-        }else{
+        } else {
             true
         }
     }
 
-    private fun validatePass(): Boolean{
+    private fun validateLastName(): Boolean {
+        lastname = etLastName.text.toString().trim()
+        return if (TextUtils.isEmpty(lastname)) {
+            Toast.makeText(this, "Entrez votre Prénom", Toast.LENGTH_SHORT).show()
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        email = etEmail.text.toString().trim()
+        return if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Entrez votre adresse mail !", Toast.LENGTH_SHORT).show()
+            false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Veuillez entrer une adresse mail valide !", Toast.LENGTH_SHORT)
+                .show()
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun validatePhone(): Boolean {
+        phonenumber = etPhoneNumber.text.toString().trim()
+        return if (TextUtils.isEmpty(phonenumber)) {
+            Toast.makeText(this, "Entrez votre Nom", Toast.LENGTH_SHORT).show()
+            false
+        } else if (!Patterns.PHONE.matcher(phonenumber).matches()) {
+            Toast.makeText(
+                this,
+                "Veuillez entrer un numéro de téléphone valide !",
+                Toast.LENGTH_SHORT
+            ).show()
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun validatePass(): Boolean {
         password = etPassword.text.toString().trim()
         coPassword = etCPassword.text.toString().trim()
 
         return when {
-            TextUtils.isEmpty(password) ->{
+            TextUtils.isEmpty(password) -> {
                 Toast.makeText(this, "Entrez votre mot de passe", Toast.LENGTH_SHORT).show()
                 false
             }
-            TextUtils.isEmpty(coPassword) ->{
+
+            TextUtils.isEmpty(coPassword) -> {
                 Toast.makeText(this, "Confirmer votre mot de passe", Toast.LENGTH_SHORT).show()
                 false
             }
-            password.length <=6 ->{
+
+            password.length <= 6 -> {
                 Toast.makeText(this, "Votre mot de passe est trop court", Toast.LENGTH_SHORT).show()
                 false
             }
+
             else -> true
 
         }
@@ -173,7 +216,7 @@ class Register : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        if(FirebaseAuth.getInstance().currentUser!=null){
+        if (FirebaseAuth.getInstance().currentUser != null) {
             val intent = Intent(this, Accueil::class.java)
             startActivity(intent)
         }
--- app/src/main/java/model/UserData.kt
+++
@@ -1,8 +0,0 @@
-package model
-
-class UserData(firstname: String, lastname: String, email: String, phonenumber: String) {
-    var FirstName: String? = null
-    var LastName: String? = null
-    var Email: String? = null
-    var PhoneNumber: String? = null
-}
