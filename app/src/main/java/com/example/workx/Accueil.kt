28ef62bbd9b2254cd29d0a9c1d1e5ca20bf191ba
Vadil <65923431+vadilkt@users.noreply.github.com>
2023-12-03 19:36:50 +0100

    Auth Ok, Create Ad Ok, Working on displaying

--- app/src/main/java/com/example/workx/Accueil.kt
+++ app/src/main/java/com/example/workx/Accueil.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +25,7 @@ import com.example.workx.model.Ad
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -39,11 +43,14 @@ class Accueil : AppCompatActivity() {
     private lateinit var searchBtn: ImageButton
     private lateinit var fabCreateAd: FloatingActionButton
     private lateinit var adapter: FirebaseRecyclerAdapter<Ad, AdViewHolder>
+    private lateinit var btnOpenSideBar : ImageButton
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accueil)
 
+        btnOpenSideBar = findViewById(R.id.btnOpenSidebar)
         searchBtn = findViewById(R.id.searchBtn)
         search_text = findViewById(R.id.search_text)
         recyclerView = findViewById(R.id.recyclerView)
@@ -51,6 +58,25 @@ class Accueil : AppCompatActivity() {
         database = FirebaseDatabase.getInstance()
         adRef = database.getReference("Ad")
         fabCreateAd = findViewById(R.id.fabCreateAd)
+        navigationView = findViewById(R.id.navigationView)
+
+        navigationView.setNavigationItemSelectedListener {menuItem->
+            when (menuItem.itemId){
+                R.id.logout->{
+                    mAuth.signOut()
+                    Toast.makeText(this,"Déconnexion réussie", Toast.LENGTH_SHORT).show()
+                    val intent= Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    return@setNavigationItemSelectedListener true
+                }
+                else-> return@setNavigationItemSelectedListener false
+            }
+        }
+
+        btnOpenSideBar.setOnClickListener{
+            openSideBar()
+        }
 
         fabCreateAd.setOnClickListener {
             val intent = Intent(this, CreateAd::class.java)
@@ -101,6 +127,11 @@ class Accueil : AppCompatActivity() {
         }
     }
 
+    private fun openSideBar() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout.openDrawer(Gravity.LEFT)
+    }
+
     private fun performSearch(query: String) {
         Log.d("TAG", "Search query: $query")
 
@@ -110,33 +141,9 @@ class Accueil : AppCompatActivity() {
         val options = FirebaseRecyclerOptions.Builder<Ad>()
             .setQuery(queryRef, Ad::class.java)
             .build()
-        adapter = object : FirebaseRecyclerAdapter<Ad, AdViewHolder>(options) {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdViewHolder {
-                val view =
-                    LayoutInflater.from(parent.context).inflate(R.layout.ad_item, parent, false)
-                return AdViewHolder(view)
-            }
 
-            override fun onBindViewHolder(holder: AdViewHolder, position: Int, model: Ad) {
-                Log.d("TAG", "Ad at position $position: $model")
-                holder.bind(model)
+        adapter.updateOptions(options)
 
-
-                holder.adBtn.setOnClickListener {
-                    val intent = Intent(this@Accueil, Ad_Detail::class.java)
-                    intent.putExtra(
-                        "adId",
-                        getRef(position).key
-                    )
-                    startActivity(intent)
-                }
-            }
-        }
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-        adapter.startListening()
     }
 
     override fun onStart() {
